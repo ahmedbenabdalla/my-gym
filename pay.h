f330a91ebef83dd1a38e48def3081c7a7177e0a6
brahimbounguicha <44786262+brahimbounguicha@users.noreply.github.com>
2018-12-11 01:13:49 +0100

    Add files via upload

---
+++ pay.h
@@ -0,0 +1,21 @@
+#ifndef PAY_H_INCLUDED
+#define PAY_H_INCLUDED
+#include <stdio.h>
+#include <string.h>
+struct Paiment
+{
+    int ID;
+    int NBMOIS;
+    char NOM[50];
+    char DATE[30];
+};
+typedef struct Paiment pmt;
+
+void ajouter_paiment(pmt);
+void modifier_paiment(pmt);
+void supprimer_paiment(int);
+void accedernom(int,char[]);
+int verif_paiment(int);
+
+
+#endif //PAIEMENT_H_INCLUDED
